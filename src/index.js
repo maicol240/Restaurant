df5b09fca141195e5f66fb2e26227b0d5fc9d71e
Maicol <72515186+maicol240@users.noreply.github.com>
2024-09-04 04:12:43 -0400

    will add navigation functionality to nav buttons

--- src/index.js
+++ src/index.js
@@ -5,10 +5,47 @@ import { loadAbout } from "./About/About";
 
 const container = document.querySelector("#content");
 
+(function setUpNavigation() {
+  const btn = document.querySelectorAll("nav button");
+  btn.forEach((btn) => {
+    //assign events to each nav
+    //home
+    if (btn.textContent == "Home") {
+      btn.addEventListener("click", () => {
+        container.innerHTML = " ";
+        container.appendChild(loadHome());
+      });
+    }
+    //Menu
+    if (btn.textContent == "Menu") {
+      btn.addEventListener("click", () => {
+        container.innerHTML = " ";
+        container.appendChild(loadMenu());
+      });
+    }
+
+    //about
+    if (btn.textContent == "About") {
+      btn.addEventListener("click", () => {
+        container.innerHTML = " ";
+        container.appendChild(loadAbout());
+      });
+    }
+  });
+})();
+
+function loadPage() {
+  container.appendChild(loadHome());
+
+  //nav btn functions
+}
+
 // //const home = createHome();
 // //
 // // container.appendChild(home);
 
 // container.appendChild(loadMenu());
 
-container.appendChild(loadAbout());
+///container.appendChild(loadAbout());
+
+loadPage();
